eeeb70df143a107b991427a851d3a6f6ff963ac3
Oliver Davies <oliver@oliverdavies.uk>
2023-03-10 22:10:07 +0000

    chore: allow for missing fields

--- src/Validator/ConfigurationValidator.php
+++ src/Validator/ConfigurationValidator.php
@@ -18,41 +18,50 @@ final class ConfigurationValidator implements ValidatorInterface
 
         $constraint = new Assert\Collection(
             [
-                'name' => [
-                    new Assert\NotNull(),
-                    new Assert\Type('string'),
-                    new Assert\Length(['min' => 1]),
+                'fields' => [
+                    'name' => [
+                        new Assert\NotNull(),
+                        new Assert\Type('string'),
+                        new Assert\Length(['min' => 1]),
+                    ],
+
+                    'language' => [
+                        new Assert\NotNull(),
+                        new Assert\Type('string'),
+                        new Assert\Choice(['php']),
+                    ],
+
+                    'type' => [
+                        new Assert\NotNull(),
+                        new Assert\Type('string'),
+                        new Assert\Choice(['drupal-project', 'php-library']),
+                    ],
+
+                    'database' => new Assert\Optional(),
+
+                    'drupal' => new Assert\Optional(),
+
+                    'docker-compose' => new Assert\Optional(),
+
+                    'dockerfile' => new Assert\Optional(),
+
+                    // TODO: this should be a boolean if present.
+                    'justfile' => new Assert\Optional(),
+
+                    'php' => new Assert\Optional(),
+
+                    'web' => new Assert\Optional(),
                 ],
 
-                'language' => [
-                    new Assert\NotNull(),
-                    new Assert\Type('string'),
-                    new Assert\Choice(['php']),
-                ],
-
-                'type' => [
-                    new Assert\NotNull(),
-                    new Assert\Type('string'),
-                    new Assert\Choice(['drupal-project', 'php-library']),
-                ],
-
-                'database' => new Assert\Optional(),
-
-                'drupal' => new Assert\Optional(),
-
-                'docker-compose' => new Assert\Optional(),
-
-                'dockerfile' => new Assert\Optional(),
-
-                // TODO: this should be a boolean if present.
-                'justfile' => new Assert\Optional(),
-
-                'php' => new Assert\Optional(),
-
-                'web' => new Assert\Optional(),
+                'allowExtraFields' => false,
+                'allowMissingFields' => true,
             ],
         );
 
-        return $validator->validate($configurationData, $constraint, $groups);
+        return $validator->validate(
+            constraints: $constraint,
+            groups: $groups,
+            value: $configurationData,
+        );
     }
 }
