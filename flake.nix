ae6155dc39712b9067965394ae9708b901641e45
Oliver Davies <oliver@oliverdavies.uk>
2023-10-06 21:54:02 +0100

    build(deps): update composer.lock

--- flake.nix
+++ flake.nix
@@ -1,15 +1,30 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
 
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devShells = {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [ php82 php82Packages.composer ];
-          };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ php82 php82Packages.composer ];
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "build-configs2";
+          src = ".";
+
+          # buildPhase = ''
+          #   echo "build"
+          #   ls
+          # '';
+
+          installPhase = ''
+            touch $out
+            # mkdir $out
+            # cp bin vendor $out
+            # ls "$out"
+          '';
         };
       };
     };
