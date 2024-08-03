a83c51bbcda7db8583ba6d6732fa7d10ac132fff
Oliver Davies <oliver@oliverdavies.dev>
2024-08-03 12:22:19 +0100

    Move console commands into a `Console` directory

--- src/Console/Command/GenerateCommand.php
+++ src/Console/Command/GenerateCommand.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Command;
+namespace App\Console\Command;
 
 use App\Action\CreateFinalConfigurationData;
 use App\Action\CreateListOfFilesToGenerate;
--- src/Console/Command/InitCommand.php
+++ src/Console/Command/InitCommand.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Command;
+namespace App\Console\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
